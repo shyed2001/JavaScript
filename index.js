8cc7fc72084bf88462d194d405f12aae73b19347
Shyed Shahriar <48835958+shyed2001@users.noreply.github.com>
2021-10-05 02:21:55 +0600

    ok
    ok

---
+++ index.js
@@ -0,0 +1,36 @@
+ //Js programming first comment with 
+ console.log("Hello World from index.Js");
+
+ var variable = "First"; // var should not be used
+ let nam = 'Name111'; // String literal
+ let number = 3; // Number Literals
+
+ let boolean_variable = false; // boolean literal
+ let first_name = undefined; // undefined literal
+ let middle_name = null;
+
+ let person = {
+     Name: 'shyed',
+     age: 30
+ }
+
+ person.age = 65;
+ person[Name] = "Shyed";
+ console.log(person.age);
+
+ console.log(person.Name);
+ console.log(number);
+ console.log(nam);
+ console.log(variable);
+
+ let interestRate = 0.3;
+
+ console.log(interestRate);
+
+ interestRate = 8;
+ console.log(interestRate);
+
+ const rate = 5; // const variable can not be changed
+ console.log(rate);
+
+ // const rate = 15; this will give errors
